4fb05ff639f1f2bcee48be6aab8e549a8a4b3f0a
mehmetbarannakipoglu <mehmetbarannakipoglu@outlook.com>
2022-09-30 14:32:54 +0300

    Initial commit: project set-up

---
+++ src/main/java/com/mehmet/earthquakefinder/EarthquakeFinderApplication.java
@@ -0,0 +1,13 @@
+package com.mehmet.earthquakefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarthquakeFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mehmet/earthquakefinder/EarthquakeFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmet.earthquakefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
